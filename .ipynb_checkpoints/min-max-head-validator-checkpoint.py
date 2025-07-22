e6addd5b50709e3a636cedd68dafecdd4049cc68
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-22 16:07:44 -0400

    Finished is_max_heap function

--- .ipynb_checkpoints/min-max-head-validator-checkpoint.py
+++ .ipynb_checkpoints/min-max-head-validator-checkpoint.py
@@ -11,3 +11,9 @@ def is_min_heap(arr):
 
 def is_max_heap(arr):
     n = len(arr)
+    for i in range((n - 2) // 2 + 1):
+        if 2 * i + 1 < n and arr[i] < arr[2 * i + 1]:
+            return False
+        if 2 * i + 2 < n and arr[i] < arr[2 * i + 2]:
+            return False
+    return True
