2f3d95b2bb6a366afb718067fc28f9b41a0991c2
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-22 16:08:33 -0400

    Initialized main function and added test case variables

--- min-max-head-validator.py
+++ min-max-head-validator.py
@@ -17,3 +17,9 @@ def is_max_heap(arr):
         if 2 * i + 2 < n and arr[i] < arr[2 * i + 2]:
             return False
     return True
+
+
+def main():
+    A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    B = [11, 9, 7, 5, 3, 1, -1, -3, -5, -7]
+    C = [6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
